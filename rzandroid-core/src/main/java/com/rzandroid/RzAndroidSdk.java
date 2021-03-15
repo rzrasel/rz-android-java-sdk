b15cea2e5f4ce8167d9d3732b3eb1f2a5aded41b
DESKTOP-KOO34JG\OMEN <rashedgit@gmail.com>
2021-03-15 12:03:25 +0600

    Create "constants.gradle", "RzAndroidSdkVersion.java" file

--- rzandroid-core/src/main/java/com/rzandroid/RzAndroidSdk.java
+++ rzandroid-core/src/main/java/com/rzandroid/RzAndroidSdk.java
@@ -1,4 +1,13 @@
 package com.rzandroid;
 
 public class RzAndroidSdk {
+//    private static volatile boolean isDebugEnabled = BuildConfig.DEBUG;
+    /**
+     * Returns the current version of the RzAndroid SDK for Android as a string.
+     *
+     * @return the current version of the SDK
+     */
+    public static String getSdkVersion() {
+        return RzAndroidSdkVersion.BUILD;
+    }
 }
---
+++ rzandroid-core/src/main/java/com/rzandroid/RzAndroidSdkVersion.java
@@ -0,0 +1,5 @@
+package com.rzandroid;
+
+final class RzAndroidSdkVersion {
+    public static final String BUILD = "9.1.0";
+}
